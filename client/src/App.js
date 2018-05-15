3ca7fda95aaa30e69ffd19a89ae58d5615ac7794
flomelki <chezwam69@hotmail.com>
2018-05-15 15:09:41 +0200

    Fixed loginbox position

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,39 @@
+// Components
 import React, { Component } from 'react';
 import LoginBox from './LoginBox';
+import ChatRoom from './ChatRoom';
+
+// CSS classes
 import './App.css';
 import './bootstrap.css';
 
 class App extends Component {
-  constructor()
-  {
+  constructor() {
     super();
 
-    this.state = { loggedState : false };
+    this.state = { loggedState: false };
+  }
 
-      // let ws = new WebSocket("ws://localhost:8066", "echo-protocol");
-      // ws.onopen = function(even) 
-      // {
-      //   ws.send('init');
-      // }
+  render() {
+    if (!this.state.loggedState) {
 
-
-    }
-    render() {
-      if (!this.state.loggedState)
-      {
-
-        return (
-          <div className='row'>
-          <div className='mx-auto'>
-          <LoginBox callback={(loggedState) => this.setLoggedState(loggedState) } />
+      return (
+        <div className='container justify-content-md-center'>
+          <div class="row justify-content-md-center">
+            <LoginBox callback={(loggedState) => this.setLoggedState(loggedState)} />
           </div>
-          </div>
-          );
-        }
-        else
-        {
-          return (<h1>TODO</h1>);
-        }
-      }
-
-      setLoggedState(loggedState)
-      {
-        console.log(loggedState)
-        this.setState({ loggedState : loggedState})
-      }
-
+        </div>
+      );
     }
+    else {
+      return (<ChatRoom callback={(loggedState) => this.setLoggedState(loggedState)} />);
+    }
+  }
 
-    export default App;
+  setLoggedState(loggedState) {
+    this.setState({ loggedState: loggedState })
+  }
+
+}
+
+export default App;
--- client/src/LoginBox.js
+++ client/src/LoginBox.js
@@ -32,32 +32,34 @@ class LoginBox extends Component {
 
   render() {
     return (
-      <div className='loginbox '>
-        <div className='row'><header className='col-md-12'><h4>Connectez-vous sur itsme!</h4></header></div>
+      <div className='loginbox col-6' >
+        <div className='row'><header className='col'><h4>Connectez-vous sur itsme!</h4></header></div>
         <div className='row'>
-          <div className="input-group mb-3 col-md-12">
+          <div className="input-group mb-3 col">
             <div className="input-group-prepend">
               <span className="input-group-text" id="username-addon">Username</span>
             </div>
             <input id='username' type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="username-addon"></input>
           </div>
+        </div>
 
-          <div className="input-group mb-3 col-md-12">
+        <div className='row'>
+          <div className="input-group mb-3 col">
             <div className="input-group-prepend">
               <span className="input-group-text" id="password-addon">Password</span>
             </div>
             <input id='password' type="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon"></input>
           </div>
+        </div>
 
-          <div className='row mx-auto'>
-            <div className="btn-group col-md-12" role="group">
-              <button type="button" className="btn btn-secondary" onClick={() => { Network.getAsyncRequest(`http://localhost:8067/users/${document.getElementById('username').value}/${document.getElementById('password').value}`, (ctx) => this.handleLogin(ctx)) }}>Login</button>
-              <button type="button" className="btn btn-secondary" onClick={() => { Network.putAsyncRequest(`http://localhost:8067/users/`, JSON.stringify({ 'username': document.getElementById('username').value, 'pwd': document.getElementById('password').value }), (ctx) => this.handleSubscribe(ctx)) }}>Subscribe</button>
-            </div>
+        <div className='row mx-auto'>
+          <div className="btn-group col-md-12" role="group">
+            <button type="button" className="btn btn-secondary" onClick={() => { Network.getAsyncRequest(`http://localhost:8067/users/${document.getElementById('username').value}/${document.getElementById('password').value}`, (ctx) => this.handleLogin(ctx)) }}>Login</button>
+            <button type="button" className="btn btn-secondary" onClick={() => { Network.putAsyncRequest(`http://localhost:8067/users/`, JSON.stringify({ 'username': document.getElementById('username').value, 'pwd': document.getElementById('password').value }), (ctx) => this.handleSubscribe(ctx)) }}>Subscribe</button>
           </div>
-          <div className='row mx-auto'>
-            <div className='col-md-12'>{this.state.loginStatus === 'nok' ? 'Loggin failed' : this.state.subscribeStatus === 'nok' ? 'Subscription failed' : this.state.subscribeStatus === 'ok' ? 'Subscription succeeded. Please now login' : ''}</div>
-          </div>
+        </div>
+        <div className='row mx-auto'>
+          <div className='col-md-12'>{this.state.loginStatus === 'nok' ? 'Loggin failed' : this.state.subscribeStatus === 'nok' ? 'Subscription failed' : this.state.subscribeStatus === 'ok' ? 'Subscription succeeded. Please now login' : ''}</div>
         </div>
       </div>
     );
