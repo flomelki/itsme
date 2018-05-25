306e7d22a79f0c3ec21e5299759cf0b0e0f0b787
flomelki <chezwam69@hotmail.com>
2018-05-25 15:28:58 +0200

    Added POST service

--- client/src/lib/network.js
+++ client/src/lib/network.js
@@ -1,7 +1,5 @@
-export default class Network
-{
-  static getAsyncRequest(url, callback)
-  {
+export default class Network {
+  static getAsyncRequest(url, callback) {
     var req = new XMLHttpRequest();
 
     req.open('GET', url, true);
@@ -10,38 +8,48 @@ export default class Network
     req.onreadystatechange = function (aEvt) {
       if (req.readyState === 4) {
         manageResponse(req, callback);
-    }
-  };
-  req.send(null);
-}
-
-static putAsyncRequest(url, body, callback)
-{
- var req = new XMLHttpRequest();
- req.open('PUT', url, true);
- req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
- req.onreadystatechange = function (aEvt) {
-  if (req.readyState === 4) {
-    manageResponse(req, callback);
+      }
+    };
+    req.send(null);
   }
-}
-req.send(body);   
-}
-} // and export
 
-function manageResponse(req, callback)
-{
-  switch(req.status)
-  {
-    case(200):
-      if (callback) callback({status : 'ok', rawResponse : req.response === 'OK' ? '' : JSON.parse(req.response) })
-        break;
-    case(204):
-      if (callback) callback({ status : 'nok' });
+  static putAsyncRequest(url, body, callback) {
+    var req = new XMLHttpRequest();
+    req.open('PUT', url, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState === 4) {
+        manageResponse(req, callback);
+      }
+    }
+    req.send(body);
+  }
+
+  static postAsyncRequest(url, body, callback) {
+    var req = new XMLHttpRequest();
+    req.open('POST', url, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState === 4) {
+        manageResponse(req, callback);
+      }
+    }
+    req.send(body);
+  }
+} // end export
+
+function manageResponse(req, callback) {
+  switch (req.status) {
+    case (200):
+      if (callback) callback({ status: 'ok', rawResponse: req.response === 'OK' ? '' : JSON.parse(req.response) })
+      break;
+    case (204):
+      if (callback) callback({ status: 'nok' });
       break;
     default:
       console.dir(req);
-    break;
+      break;
   }
 }
